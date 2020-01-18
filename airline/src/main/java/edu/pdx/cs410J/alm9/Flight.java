859965ae1cb8ad9802e0f7a879ce855e6ffc85cb
Alexis M <alexis.murauskas@gmail.com>
2020-01-18 13:57:04 -0800

    Another reformat

--- airline/src/main/java/edu/pdx/cs410J/alm9/Flight.java
+++ airline/src/main/java/edu/pdx/cs410J/alm9/Flight.java
@@ -1,42 +1,50 @@
 package edu.pdx.cs410J.alm9;
 
 import edu.pdx.cs410J.AbstractFlight;
+
 import java.time.LocalDateTime;
 
 public class Flight extends AbstractFlight {
 
-  private int flightNumber;
-  private String source;
-  private LocalDateTime departureTime;
-  private String destination;
-  private LocalDateTime arrivalTime;
+    private int flightNumber;
+    private String source;
+    private LocalDateTime departureTime;
+    private String destination;
+    private LocalDateTime arrivalTime;
 
-  public Flight() {}
+    public Flight() {
+    }
 
-  public Flight(int flightNumber, String source, LocalDateTime departureTime, String destination, LocalDateTime arrivalTime) {
-    this.flightNumber = flightNumber;
-    this.source = source;
-    this.departureTime = departureTime;
-    this.destination = destination;
-    this.arrivalTime = arrivalTime;
-  }
+    public Flight(int flightNumber, String source, LocalDateTime departureTime, String destination, LocalDateTime arrivalTime) {
+        this.flightNumber = flightNumber;
+        this.source = source;
+        this.departureTime = departureTime;
+        this.destination = destination;
+        this.arrivalTime = arrivalTime;
+    }
 
-  @Override
-  public int getNumber() {
-    return this.flightNumber;
-  }
+    @Override
+    public int getNumber() {
+        return this.flightNumber;
+    }
 
-  @Override
-  public String getSource() { return this.source; }
+    @Override
+    public String getSource() {
+        return this.source;
+    }
 
-  @Override
-  public String getDepartureString() { return this.departureTime.toString(); }
+    @Override
+    public String getDepartureString() {
+        return this.departureTime.toString();
+    }
 
-  @Override
-  public String getDestination() { return this.destination; }
+    @Override
+    public String getDestination() {
+        return this.destination;
+    }
 
-  @Override
-  public String getArrivalString() {
-    return this.arrivalTime.toString();
-  }
+    @Override
+    public String getArrivalString() {
+        return this.arrivalTime.toString();
+    }
 }
--- airline/src/test/java/edu/pdx/cs410J/alm9/Airline.java
+++ airline/src/test/java/edu/pdx/cs410J/alm9/Airline.java
@@ -1,4 +1,30 @@
 package edu.pdx.cs410J.alm9;
 
-public class Airline {
+import edu.pdx.cs410J.AbstractAirline;
+import edu.pdx.cs410J.AbstractFlight;
+
+import java.util.Collection;
+
+public class Airline extends AbstractAirline {
+    private String name;
+    private Collection<Flight> flights;
+
+    public Airline(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public void addFlight(AbstractFlight abstractFlight) {
+        this.flights.add((Flight) abstractFlight);
+    }
+
+    @Override
+    public Collection getFlights() {
+        return this.flights;
+    }
 }
