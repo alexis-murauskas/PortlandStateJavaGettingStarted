2a1e51b1babe6118a86bee382347ef39d000717e
Alexis M <alexis.murauskas@gmail.com>
2020-01-28 18:36:39 -0800

    Style refactor

--- airline/src/test/java/edu/pdx/cs410J/alm9/TextDumperTest.java
+++ airline/src/test/java/edu/pdx/cs410J/alm9/TextDumperTest.java
@@ -1,14 +1,26 @@
 package edu.pdx.cs410J.alm9;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.MatcherAssert.assertThat;
-
 import org.junit.Test;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
 
 public class TextDumperTest {
+    private Flight flight1 = new Flight(
+            1,
+            "SRC",
+            LocalDateTime.parse("11/11/1111 11:11", Flight.DATEFORMAT),
+            "DST",
+            LocalDateTime.parse("12/12/1212 12:12", Flight.DATEFORMAT)
+    );
+    private Flight flight2 = new Flight(
+            2,
+            "SRC2",
+            LocalDateTime.parse("11/11/1111 11:11", Flight.DATEFORMAT),
+            "DST2",
+            LocalDateTime.parse("12/12/1212 12:12", Flight.DATEFORMAT)
+    );
+
     @Test
     public void nullArgThrowsException() {
         TextDumper sut = new TextDumper();
@@ -22,17 +34,9 @@ public class TextDumperTest {
 
     @Test
     public void validArgDoesNotThrowException() {
-        Flight flight = new Flight(
-                1,
-                "SRC",
-                LocalDateTime.parse("11/11/1111 11:11", Flight.DATEFORMAT),
-                "DST",
-                LocalDateTime.parse("12/12/1212 12:12", Flight.DATEFORMAT)
-        );
-
         Airline<Flight> airline = new Airline("Airline");
         TextDumper<Airline<Flight>, Flight> sut = new TextDumper();
-        airline.addFlight(flight);
+        airline.addFlight(flight1);
 
         try {
             sut.dump(airline);
@@ -46,21 +50,8 @@ public class TextDumperTest {
         Airline<Flight> airline = new Airline("MultipleFlights");
         TextDumper<Airline<Flight>, Flight> sut = new TextDumper();
 
-        airline.addFlight(new Flight(
-                1,
-                "SRC",
-                LocalDateTime.parse("11/11/1111 11:11", Flight.DATEFORMAT),
-                "DST",
-                LocalDateTime.parse("12/12/1212 12:12", Flight.DATEFORMAT)
-        ));
-
-        airline.addFlight(new Flight(
-                2,
-                "SRC2",
-                LocalDateTime.parse("11/11/1111 11:11", Flight.DATEFORMAT),
-                "DST2",
-                LocalDateTime.parse("12/12/1212 12:12", Flight.DATEFORMAT)
-        ));
+        airline.addFlight(flight1);
+        airline.addFlight(flight2);
 
         try {
             sut.dump(airline);
@@ -69,4 +60,18 @@ public class TextDumperTest {
         }
     }
 
+    @Test
+    public void airlineWithLongName() {
+        Airline<Flight> airline = new Airline("'Long Name'");
+        TextDumper<Airline<Flight>, Flight> sut = new TextDumper();
+
+        airline.addFlight(flight1);
+        airline.addFlight(flight2);
+
+        try {
+            sut.dump(airline);
+        } catch (IOException e) {
+            assert (false);
+        }
+    }
 }
