91a393bf8d113278c2d5e6917d84d406c7f3578b
Alexis M <alexis.murauskas@gmail.com>
2020-02-05 12:19:06 -0800

    IT for project3

--- airline/src/test/java/edu/pdx/cs410J/alm9/Project2Test.java
+++ airline/src/test/java/edu/pdx/cs410J/alm9/Project2Test.java
@@ -1,13 +1,28 @@
 package edu.pdx.cs410J.alm9;
 
+import edu.pdx.cs410J.InvokeMainTestCase;
 import org.junit.Test;
 
-import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-public class Project2Test {
+public class Project2Test extends InvokeMainTestCase {
+
+    /**
+     * Invokes the main method of {@link Project2} with the given arguments.
+     */
+    private InvokeMainTestCase.MainMethodResult invokeMain(String... args) {
+        return invokeMain(Project2.class, args);
+    }
+
+    @Test
+    public void checkReadme() {
+        assertThat(Project2.readme.contains("-textFile"), is(true));
+    }
+
     @Test
     public void callMain() {
-        assertThat(Project2.readme,  containsString("Alexis Murauskas"));
+        var rv = invokeMain(new String[]{"-README"});
+        assertThat(rv.getExitCode(), is(0));
     }
 }
--- airline/src/test/java/edu/pdx/cs410J/alm9/Project3Test.java
+++ airline/src/test/java/edu/pdx/cs410J/alm9/Project3Test.java
@@ -3,21 +3,21 @@ package edu.pdx.cs410J.alm9;
 import edu.pdx.cs410J.InvokeMainTestCase;
 import org.junit.Test;
 
-import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-public class Project1Test extends InvokeMainTestCase {
+public class Project3Test extends InvokeMainTestCase {
 
     /**
-     * Invokes the main method of {@link Project1} with the given arguments.
+     * Invokes the main method of {@link Project3} with the given arguments.
      */
-    private MainMethodResult invokeMain(String... args) {
-        return invokeMain(Project1.class, args);
+    private InvokeMainTestCase.MainMethodResult invokeMain(String... args) {
+        return invokeMain(Project3.class, args);
     }
 
     @Test
     public void checkReadme() {
-        assertThat(Project1.readme.contains("Alexis Murauskas"), is(true));
+        assertThat(Project3.readme.contains("-pretty"), is(true));
     }
 
     @Test
