f8cc42edfc719019af0b0a250f1fb1d1b559e15d
Alexis M <alexis.murauskas@gmail.com>
2020-02-17 21:55:02 -0800

    Implemented parser

--- airline/src/main/java/edu/pdx/cs410J/alm9/XmlParser.java
+++ airline/src/main/java/edu/pdx/cs410J/alm9/XmlParser.java
@@ -1,9 +1,20 @@
 package edu.pdx.cs410J.alm9;
 
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Collection;
+
 import edu.pdx.cs410J.AbstractAirline;
 import edu.pdx.cs410J.AbstractFlight;
 import edu.pdx.cs410J.AirlineParser;
 import edu.pdx.cs410J.ParserException;
+import org.w3c.dom.*;
+import org.xml.sax.SAXException;
 
 public class XmlParser<T extends AbstractAirline<Q>, Q extends AbstractFlight> implements AirlineParser<T> {
     private String fileName;
@@ -14,6 +25,83 @@ public class XmlParser<T extends AbstractAirline<Q>, Q extends AbstractFlight> i
 
     @Override
     public T parse() throws ParserException {
-        return null;
+        Airline<Flight> airline;
+        try {
+            Document document = createDocument();
+            airline = processAirlineName(document);
+            Collection<InputModel> flights = processFlights(document);
+
+            for (var flight : flights)
+                airline.addFlight(flight);
+        } catch (Exception e) {
+            throw new ParserException(e.toString());
+        }
+
+        return (T) airline;
+    }
+
+    private Document createDocument() throws IOException, SAXException, ParserConfigurationException {
+        File in = new File(fileName);
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document document = builder.parse(in);
+        document.getDocumentElement().normalize();
+
+        return document;
+    }
+
+    private Airline processAirlineName(Document document) {
+        NodeList airlineElement = document.getElementsByTagName("name");
+        String name = airlineElement.item(0).getTextContent();
+
+        if (name.contains(" "))
+            name = "'" + name + "'";
+
+        return new Airline<Flight>(name);
+    }
+
+    private Collection<InputModel> processFlights(Document document) throws ParseException {
+        ArrayList<InputModel> flights = new ArrayList<>();
+        NodeList nodes = document.getElementsByTagName("flight");
+
+        for (int i = 0; i < nodes.getLength(); ++i) {
+            var rv = processFlight(nodes.item(i));
+            flights.add(rv);
+        }
+
+        return flights;
+    }
+
+    private InputModel processFlight(Node node) throws ParseException {
+        Element flight = (Element) node;
+        ArrayList<String> in = new ArrayList<>();
+
+        in.add("placeholder");
+        in.add(flight.getElementsByTagName("number").item(0).getTextContent());
+
+        in.add(flight.getElementsByTagName("src").item(0).getTextContent());
+        String departString = processDateTime(flight.getElementsByTagName("depart"));
+        String[] depart = departString.split(";");
+        in.add(depart[0]);
+        in.add(depart[1]);
+        in.add(depart[2]);
+
+        in.add(flight.getElementsByTagName("dest").item(0).getTextContent());
+        String arriveString =  processDateTime(flight.getElementsByTagName("arrive"));
+        String[] arrive = arriveString.split(";");
+        in.add(arrive[0]);
+        in.add(arrive[1]);
+        in.add(arrive[2]);
+
+        String[] array = new String[in.size()];
+        in.toArray(array);
+
+        return AirlineCommand.parse(array);
+    }
+
+    private String processDateTime(NodeList list) {
+        String rv = "2/2/2020;4:00;pm";
+
+        return rv;
     }
 }
--- airline/src/test/java/edu/pdx/cs410J/alm9/XmlParserTest.java
+++ airline/src/test/java/edu/pdx/cs410J/alm9/XmlParserTest.java
@@ -1,4 +1,39 @@
 package edu.pdx.cs410J.alm9;
 
+import edu.pdx.cs410J.ParserException;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.CoreMatchers.*;
+
 public class XmlParserTest {
+
+    @Test
+    public void parserCanReadInValidFile() throws ParserException {
+        XmlParser<Airline<Flight>, Flight> parser = new XmlParser<>("valid-airline.xml");
+        parser.parse();
+    }
+
+    @Test (expected = ParserException.class)
+    public void parserThrowsExceptionForInvalidFIle() throws ParserException {
+        XmlParser<Airline<Flight>, Flight> parser = new XmlParser<>("invalid-airline.xml");
+        parser.parse();
+    }
+
+    @Test
+    public void airlineHasCorrectName() throws ParserException {
+        XmlParser<Airline<Flight>, Flight> parser = new XmlParser<>("valid-airline.xml");
+        Airline<Flight> rv = parser.parse();
+
+        assertThat(!rv.getName().isEmpty(), is(true));
+        assertThat(rv.getName(), is("'Valid Airlines'"));
+    }
+
+    @Test
+    public void airlineHasCorrectNumberOfFlights() throws ParserException {
+        XmlParser<Airline<Flight>, Flight> parser = new XmlParser<>("valid-airline.xml");
+        Airline<Flight> rv = parser.parse();
+
+        assertThat(rv.getFlights().size(), is(2));
+    }
 }
