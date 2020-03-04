bfccaed6a7746b5d639bb08216de19e5e22cb617
Alexis M <alexis.murauskas@gmail.com>
2020-03-03 22:06:56 -0800

    Backend implementation done

--- airline-web/src/main/java/edu/pdx/cs410J/alm9/AirlineServlet.java
+++ airline-web/src/main/java/edu/pdx/cs410J/alm9/AirlineServlet.java
@@ -1,6 +1,7 @@
 package edu.pdx.cs410J.alm9;
 
 import com.google.common.annotations.VisibleForTesting;
+import edu.pdx.cs410J.web.HttpRequestHelper;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -8,8 +9,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * This servlet ultimately provides a REST API for working with an
@@ -18,149 +25,158 @@ import java.util.Map;
  * and their definitions.
  */
 public class AirlineServlet extends HttpServlet {
-  static final String WORD_PARAMETER = "word";
-  static final String DEFINITION_PARAMETER = "definition";
+    public final static String AIRLINE = "airline";
+    public final static String FLIGHT_NO = "flight_number";
+    public final static String SRC = "src";
+    public final static String DEPART = "depart";
+    public final static String DEST = "dest";
+    public final static String ARRIVE = "arrive";
 
-  private final Map<String, String> dictionary = new HashMap<>();
+    private List<Airline<Flight>> airlines = new ArrayList<>();
 
-  /**
-   * Handles an HTTP GET request from a client by writing the definition of the
-   * word specified in the "word" HTTP parameter to the HTTP response.  If the
-   * "word" parameter is not specified, all of the entries in the dictionary
-   * are written to the HTTP response.
-   */
-  @Override
-  protected void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException
-  {
-      response.setContentType( "text/plain" );
+    /**
+     * Handles an HTTP GET request from a client by writing the definition of the
+     * word specified in the "word" HTTP parameter to the HTTP response.  If the
+     * "word" parameter is not specified, all of the entries in the dictionary
+     * are written to the HTTP response.
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/plain");
+        var airline = getParameter(AIRLINE, request);
+        var src = getParameter(SRC, request);
+        var dest = getParameter(DEST, request);
 
-      String word = getParameter( WORD_PARAMETER, request );
-      if (word != null) {
-          writeDefinition(word, response);
+        if (airline == null) {
+            missingRequiredParameter(response, AIRLINE);
+            return;
+        }
 
-      } else {
-          writeAllDictionaryEntries(response);
-      }
-  }
+        try {
+            var found = findAirline(airline);
 
-  /**
-   * Handles an HTTP POST request by storing the dictionary entry for the
-   * "word" and "definition" request parameters.  It writes the dictionary
-   * entry to the HTTP response.
-   */
-  @Override
-  protected void doPost( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException
-  {
-      response.setContentType( "text/plain" );
+            if (found != null && src != null && dest != null) {
+                var flights = found.getFlights().stream()
+                        .filter(f -> f.getSource().equals(src) && f.getDestination().equals(dest))
+                        .collect(Collectors.toList());
 
-      String word = getParameter(WORD_PARAMETER, request );
-      if (word == null) {
-          missingRequiredParameter(response, WORD_PARAMETER);
-          return;
-      }
+                found = new Airline<>(airline);
+                for (var flight : flights) {
+                    found.addFlight(flight);
+                }
+            }
 
-      String definition = getParameter(DEFINITION_PARAMETER, request );
-      if ( definition == null) {
-          missingRequiredParameter( response, DEFINITION_PARAMETER );
-          return;
-      }
-
-      this.dictionary.put(word, definition);
-
-      PrintWriter pw = response.getWriter();
-      pw.println(Messages.definedWordAs(word, definition));
-      pw.flush();
-
-      response.setStatus( HttpServletResponse.SC_OK);
-  }
-
-  /**
-   * Handles an HTTP DELETE request by removing all dictionary entries.  This
-   * behavior is exposed for testing purposes only.  It's probably not
-   * something that you'd want a real application to expose.
-   */
-  @Override
-  protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-      response.setContentType("text/plain");
-
-      this.dictionary.clear();
-
-      PrintWriter pw = response.getWriter();
-      pw.println(Messages.allDictionaryEntriesDeleted());
-      pw.flush();
-
-      response.setStatus(HttpServletResponse.SC_OK);
-
-  }
-
-  /**
-   * Writes an error message about a missing parameter to the HTTP response.
-   *
-   * The text of the error message is created by {@link Messages#missingRequiredParameter(String)}
-   */
-  private void missingRequiredParameter( HttpServletResponse response, String parameterName )
-      throws IOException
-  {
-      String message = Messages.missingRequiredParameter(parameterName);
-      response.sendError(HttpServletResponse.SC_PRECONDITION_FAILED, message);
-  }
-
-  /**
-   * Writes the definition of the given word to the HTTP response.
-   *
-   * The text of the message is formatted with
-   * {@link Messages#formatDictionaryEntry(String, String)}
-   */
-  private void writeDefinition(String word, HttpServletResponse response) throws IOException {
-    String definition = this.dictionary.get(word);
-
-    if (definition == null) {
-      response.setStatus(HttpServletResponse.SC_NOT_FOUND);
-
-    } else {
-      PrintWriter pw = response.getWriter();
-      pw.println(Messages.formatDictionaryEntry(word, definition));
-
-      pw.flush();
-
-      response.setStatus(HttpServletResponse.SC_OK);
+            dumpAirline(found, response);
+        }
+        catch (Exception e) {
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST, e.getMessage());
+        }
     }
-  }
 
-  /**
-   * Writes all of the dictionary entries to the HTTP response.
-   *
-   * The text of the message is formatted with
-   * {@link Messages#formatDictionaryEntry(String, String)}
-   */
-  private void writeAllDictionaryEntries(HttpServletResponse response ) throws IOException
-  {
-      PrintWriter pw = response.getWriter();
-      Messages.formatDictionaryEntries(pw, dictionary);
+    /**
+     * Handles an HTTP POST request by storing the dictionary entry for the
+     * "word" and "definition" request parameters.  It writes the dictionary
+     * entry to the HTTP response.
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/plain");
+        var airline = getParameter(AIRLINE, request);
+        var flightNo = getParameter(FLIGHT_NO, request);
+        var src = getParameter(SRC, request);
+        var depart = getParameter(DEPART, request);
+        var dest = getParameter(DEST, request);
+        var arrive = getParameter(ARRIVE, request);
+        SimpleDateFormat formatter = Flight.PARSEFORMAT;
 
-      pw.flush();
+        try {
+            var found = findAirline(airline);
+            if (found == null)
+                found = new Airline<>(airline);
 
-      response.setStatus( HttpServletResponse.SC_OK );
-  }
+            var toAdd = new Flight(
+                    Integer.parseInt(flightNo),
+                    src,
+                    formatter.parse(depart),
+                    dest,
+                    formatter.parse(arrive)
+            );
 
-  /**
-   * Returns the value of the HTTP request parameter with the given name.
-   *
-   * @return <code>null</code> if the value of the parameter is
-   *         <code>null</code> or is the empty string
-   */
-  private String getParameter(String name, HttpServletRequest request) {
-    String value = request.getParameter(name);
-    if (value == null || "".equals(value)) {
-      return null;
+            found.addFlight(toAdd);
 
-    } else {
-      return value;
+            if (!airlines.contains(found))
+                airlines.add(found);
+
+            dumpAirline(found, response);
+        }
+        catch (Exception e) {
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST, e.getMessage());
+        }
     }
-  }
 
-  @VisibleForTesting
-  String getDefinition(String word) {
-      return this.dictionary.get(word);
-  }
+    private Airline<Flight> findAirline(String name) {
+        var rv = airlines.stream()
+                .filter(a -> a.getName().equals(name))
+                .findAny();
+
+        if (rv.isPresent())
+            return rv.get();
+
+        return null;
+    }
+
+    private void dumpAirline(Airline<Flight> airline, HttpServletResponse response) throws IOException {
+        var pw = response.getWriter();
+        XmlDumper<Airline<Flight>, Flight> dumper = new XmlDumper<>("response.xml");
+
+        dumper.dump(airline);
+        var message = new String(Files.readAllBytes(Paths.get("response.xml")));
+        pw.println(message);
+        pw.flush();
+
+        response.setStatus(HttpServletResponse.SC_OK);
+    }
+
+    /**
+     * Handles an HTTP DELETE request by removing all dictionary entries.  This
+     * behavior is exposed for testing purposes only.  It's probably not
+     * something that you'd want a real application to expose.
+     */
+    @Override
+    protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/plain");
+
+        PrintWriter pw = response.getWriter();
+        pw.println("Delete functionality not available");
+        pw.flush();
+
+        response.setStatus(HttpServletResponse.SC_OK);
+    }
+
+    /**
+     * Writes an error message about a missing parameter to the HTTP response.
+     * <p>
+     * The text of the error message is created by {@link Messages#missingRequiredParameter(String)}
+     */
+    private void missingRequiredParameter(HttpServletResponse response, String parameterName)
+            throws IOException {
+        String message = Messages.missingRequiredParameter(parameterName);
+        response.sendError(HttpServletResponse.SC_PRECONDITION_FAILED, message);
+    }
+
+    /**
+     * Returns the value of the HTTP request parameter with the given name.
+     *
+     * @return <code>null</code> if the value of the parameter is
+     * <code>null</code> or is the empty string
+     */
+    private String getParameter(String name, HttpServletRequest request) {
+        String value = request.getParameter(name);
+        if (value == null || "".equals(value)) {
+            return null;
+
+        } else {
+            return value;
+        }
+    }
 }
